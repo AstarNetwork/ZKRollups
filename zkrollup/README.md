d5c305e849c8fa026537b2b426b97a9a0fe2a357
NoCtrlZ <phantomofrotten@gmail.com>
2020-12-04 13:18:05 +0900

    solidity test contract

---
+++ zkrollup/README.md
@@ -0,0 +1,5 @@
+# ZK Rollup Test On Substrate
+## Test
+```
+$ npm run test
+```
---
+++ zkrollup/contracts/Test.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.6.0;
+
+contract Test {
+    function double(uint256 num) public view returns(uint256) {
+        return num * 2;
+    }
+}
