57a70fd5a439827aa89dd34dc38b2ef1b9e4f813
NoCtrlZ <phantomofrotten@gmail.com>
2021-01-01 07:53:36 +0900

    set operator env var

--- zkrollup/scripts/build_containers.sh
+++ zkrollup/scripts/build_containers.sh
@@ -7,3 +7,4 @@ cp -r build zksync/contracts/build
 echo "Build Server And Prover..."
 DOCKER_BUILDKIT=1 docker build --ssh default -f zksync/docker/server/Dockerfile zksync -t zksync/server
 DOCKER_BUILDKIT=1 docker build --ssh default -f zksync/docker/prover/Dockerfile zksync -t zksync/prover
+docker build -f docker/postgres/Dockerfile zksync/core/lib/storage/migrations -t zksync/postgres
