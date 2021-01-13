39a4b1af29c371dcec02ff38764a6c204e6c5090
NoCtrlZ <phantomofrotten@gmail.com>
2021-01-13 14:16:53 +0900

    change web3 url

--- ts-tests/tests/util.ts
+++ ts-tests/tests/util.ts
@@ -1,11 +1,9 @@
 import Web3 from "web3";
 import { JsonRpcResponse } from "web3-core-helpers";
-import { spawn, ChildProcess } from "child_process";
 
-export const PORT = 19931;
-export const RPC_PORT = 19932;
-export const WS_PORT = 19933;
-export const SPECS_PATH = `./frontier-test-specs`;
+export const PORT = 2000;
+export const RPC_PORT = 3000;
+export const WS_PORT = 4000;
 
 export const DISPLAY_LOG = process.env.FRONTIER_LOG || false;
 export const FRONTIER_LOG = process.env.FRONTIER_LOG || "info";
@@ -45,98 +43,14 @@ export async function createAndFinalizeBlock(web3: Web3) {
 	}
 }
 
-export async function startFrontierNode(specFilename: string, provider?: string): Promise<{ web3: Web3; binary: ChildProcess }> {
-
-	var web3;
-	if (!provider || provider == 'http') {
-		web3 = new Web3(`http://localhost:${RPC_PORT}`);
-	}
-
-	const cmd = BINARY_PATH;
-	const args = [
-		`--chain=${SPECS_PATH}/${specFilename}`,
-		`--validator`, // Required by manual sealing to author the blocks
-		`--execution=Native`, // Faster execution using native
-		`--no-telemetry`,
-		`--no-prometheus`,
-		`--sealing=Manual`,
-		`--no-grandpa`,
-		`--force-authoring`,
-		`-l${FRONTIER_LOG}`,
-		`--port=${PORT}`,
-		`--rpc-port=${RPC_PORT}`,
-		`--ws-port=${WS_PORT}`,
-		`--tmp`,
-	];
-	const binary = spawn(cmd, args);
-
-	binary.on("error", (err) => {
-		if ((err as any).errno == "ENOENT") {
-			console.error(
-				`\x1b[31mMissing Frontier binary (${BINARY_PATH}).\nPlease compile the Frontier project:\ncargo build\x1b[0m`
-			);
-		} else {
-			console.error(err);
-		}
-		process.exit(1);
-	});
-
-	const binaryLogs = [];
-	await new Promise((resolve) => {
-		const timer = setTimeout(() => {
-			console.error(`\x1b[31m Failed to start Frontier Template Node.\x1b[0m`);
-			console.error(`Command: ${cmd} ${args.join(" ")}`);
-			console.error(`Logs:`);
-			console.error(binaryLogs.map((chunk) => chunk.toString()).join("\n"));
-			process.exit(1);
-		}, SPAWNING_TIME - 2000);
-
-		const onData = async (chunk) => {
-			if (DISPLAY_LOG) {
-				console.log(chunk.toString());
-			}
-			binaryLogs.push(chunk);
-			if (chunk.toString().match(/Manual Seal Ready/)) {
-				if (!provider || provider == "http") {
-					// This is needed as the EVM runtime needs to warmup with a first call
-					await web3.eth.getChainId();
-				}
-
-				clearTimeout(timer);
-				if (!DISPLAY_LOG) {
-					binary.stderr.off("data", onData);
-					binary.stdout.off("data", onData);
-				}
-				// console.log(`\x1b[31m Starting RPC\x1b[0m`);
-				resolve();
-			}
-		};
-		binary.stderr.on("data", onData);
-		binary.stdout.on("data", onData);
-	});
-
-	if (provider == 'ws') {
-		web3 = new Web3(`ws://localhost:${WS_PORT}`);
-	}
-
-	return { web3, binary };
-}
-
 export function describeWithFrontier(title: string, specFilename: string, cb: (context: { web3: Web3 }) => void, provider?: string) {
 	describe(title, () => {
 		let context: { web3: Web3 } = { web3: null };
-		let binary: ChildProcess;
 		// Making sure the Frontier node has started
 		before("Starting Frontier Test Node", async function () {
 			this.timeout(SPAWNING_TIME);
-			const init = await startFrontierNode(specFilename, provider);
-			context.web3 = init.web3;
-			binary = init.binary;
-		});
-
-		after(async function () {
-			//console.log(`\x1b[31m Killing RPC\x1b[0m`);
-			binary.kill();
+			const web3 = new Web3(`http://substrate:${RPC_PORT}`)
+			context.web3 = web3;
 		});
 
 		cb(context);
