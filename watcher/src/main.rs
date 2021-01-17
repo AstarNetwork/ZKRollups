904f7cf104cc5d3efda1a5547450c6d7fd006055
NoCtrlZ <phantomofrotten@gmail.com>
2021-01-17 11:27:22 +0900

    rust web3 test

---
+++ watcher/src/main.rs
@@ -0,0 +1,21 @@
+use web3::{transports, Web3};
+use tokio;
+
+#[tokio::main]
+async fn main() -> web3::Result<()> {
+    let transport = transports::Http::new("http://localhost:8545")?;
+    let web3 = Web3::new(transport);
+
+    println!("Calling accounts.");
+    let mut accounts = web3.eth().accounts().await?;
+    println!("Accounts: {:?}", accounts);
+    accounts.push("00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap());
+
+    println!("Calling balance.");
+    for account in accounts {
+        let balance = web3.eth().balance(account, None).await?;
+        println!("Balance of {:?}: {}", account, balance);
+    }
+
+    Ok(())
+}
