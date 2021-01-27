476cb839380ec250acb545f6b8d80e960d1c916b
NoCtrlZ <phantomofrotten@gmail.com>
2021-01-27 12:53:55 +0900

    refactoring overall

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -4,14 +4,11 @@ echo "Submodule Cloning..."
 git submodule update --init --recursive
 
 echo "Copy Contract File..."
-rm -rf zksync/contracts/build
-mkdir zksync/contracts/build
 (
 cd ts-tests
 yarn
 yarn build
 )
-cp -r ts-tests/build/* zksync/contracts/build/
 
 echo "Build Server And Prover..."
 DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/server/Dockerfile zksync -t zksync/server
