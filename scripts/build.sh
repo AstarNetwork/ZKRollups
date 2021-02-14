5e5bf24c17076b8f41bbf25bdcf68320c9a7ea92
NoCtrlZ <phantomofrotten@gmail.com>
2021-02-14 19:11:58 +0900

    change repository name lowercase

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,15 +4,15 @@ echo "Submodule Cloning..."
 git submodule update --init --recursive
 
 echo "Build Container Images..."
-DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/operator/Dockerfile zksync -t docker.pkg.github.com/PlasmNetwork/zkrollups/operator:latest
-DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t docker.pkg.github.com/PlasmNetwork/zkrollups/prover:latest
-docker build blockchain -t docker.pkg.github.com/PlasmNetwork/zkrollups/substrate:latest
-docker build zksync/core/lib/storage/migrations -t docker.pkg.github.com/PlasmNetwork/zkrollups/postgres:latest
-docker build ts-tests -t docker.pkg.github.com/PlasmNetwork/zkrollups/ts-tests:latest
+DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/operator/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
+DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
+docker build blockchain -t docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
+docker build zksync/core/lib/storage/migrations -t docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
+docker build ts-tests -t docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
 
 echo "Push Container Images..."
-docker push docker.pkg.github.com/PlasmNetwork/zkrollups/operator:latest
-docker push docker.pkg.github.com/PlasmNetwork/zkrollups/prover:latest
-docker push docker.pkg.github.com/PlasmNetwork/zkrollups/substrate:latest
-docker push docker.pkg.github.com/PlasmNetwork/zkrollups/postgres:latest
-docker push docker.pkg.github.com/PlasmNetwork/zkrollups/ts-tests:latest
+docker push docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
+docker push docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
+docker push docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
+docker push docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
+docker push docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
