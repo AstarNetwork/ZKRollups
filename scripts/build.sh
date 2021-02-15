a6dc30a16063b3d0a7098d9ed7a503ed3838ff01
NoCtrlZ <phantomofrotten@gmail.com>
2021-02-15 12:28:07 +0900

    add pulling sequence

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,11 @@
 echo "Submodule Cloning..."
 git submodule update --init --recursive
 
+echo "Prepare Substrate Container Image..."
+docker build blockchain -t docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
+docker push docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
+docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/substrate
+
 echo "Prepare Operator Container Image..."
 DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/operator/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
 docker push docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
@@ -12,11 +17,6 @@ echo "Prepare Prover Contaienr Image..."
 DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
 docker push docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
 docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/prover
-
-echo "Prepare Substrate Container Image..."
-docker build blockchain -t docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
-docker push docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
-docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/substrate
 
 echo "Prepare Postgres And Integration Container Images..."
 docker build zksync/core/lib/storage/migrations -t docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
--- scripts/integration.sh
+++ scripts/integration.sh
@@ -7,6 +7,14 @@ yarn
 yarn build
 )
 
+echo "Pull Container Images..."
+docker pull docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest \
+            docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest \
+            docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest \
+            docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest \
+            docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest \
+            matterlabs/dev-ticker:latest
+
 echo "Start Integration Test..."
 docker-compose -f docker-compose.test.yml up -d substrate operator prover postgres ticker
 docker-compose -f docker-compose.test.yml up test
