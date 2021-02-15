cb3c5f0be5e9c29c9f7f510a757c63d793bcc2b6
NoCtrlZ <phantomofrotten@gmail.com>
2021-02-15 11:07:49 +0900

    remove image after for each build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,16 +3,20 @@
 echo "Submodule Cloning..."
 git submodule update --init --recursive
 
-echo "Build Container Images..."
+echo "Prepare Operator And Prover Container Images..."
 DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/operator/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
 DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
-docker build blockchain -t docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
-docker build zksync/core/lib/storage/migrations -t docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
-docker build ts-tests -t docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
-
-echo "Push Container Images..."
 docker push docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
 docker push docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
+docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/operator docker.pkg.github.com/plasmnetwork/zkrollups/prover
+
+echo "Prepare Substrate Container Image..."
+docker build blockchain -t docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
 docker push docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
+docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/substrate
+
+echo "Prepare Postgres And Integration Container Images..."
+docker build zksync/core/lib/storage/migrations -t docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
+docker build ts-tests -t docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
 docker push docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
 docker push docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
