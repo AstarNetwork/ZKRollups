23dc0a295cebc908d19a8377fe2be21688f8ed55
NoCtrlZ <phantomofrotten@gmail.com>
2021-02-15 18:47:03 +0900

    local and actions condition branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,23 +3,29 @@
 echo "Submodule Cloning..."
 git submodule update --init --recursive
 
-echo "Prepare Substrate Container Image..."
-docker build blockchain -t docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
-docker push docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
-docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/substrate
+if [[ $1 = "actions" ]]; then
+    echo "Prepare Substrate Container Image..."
+    docker build blockchain -t docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
+    docker push docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
+    docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/substrate
 
-echo "Prepare Operator Container Image..."
-DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/server/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
-docker push docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
-docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/operator
+    echo "Prepare Operator Container Image..."
+    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/server/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
+    docker push docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
+    docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/operator
 
-echo "Prepare Prover Contaienr Image..."
-DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
-docker push docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
-docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/prover
+    echo "Prepare Prover Contaienr Image..."
+    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
+    docker push docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
+    docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/prover
 
-echo "Prepare Postgres And Integration Container Images..."
-docker build zksync/core/lib/storage/migrations -t docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
-docker build ts-tests -t docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
-docker push docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
-docker push docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
+    echo "Prepare Postgres And Integration Container Images..."
+    docker build zksync/core/lib/storage/migrations -t docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
+    docker build ts-tests -t docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
+    docker push docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
+    docker push docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
+else
+    echo "Build Server And Prover..."
+    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/server/Dockerfile zksync -t zksync/server
+    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t zksync/prover
+fi
--- scripts/integration.sh
+++ scripts/integration.sh
@@ -7,14 +7,21 @@ yarn
 yarn build
 )
 
-echo "Pull Container Images..."
-docker pull docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
-docker pull docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
-docker pull docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
-docker pull docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
-docker pull docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
-docker pull matterlabs/dev-ticker:latest
+if [[ $1 = "actions" ]]; then
+    echo "Pull Container Images..."
+    docker pull docker.pkg.github.com/plasmnetwork/zkrollups/substrate:latest
+    docker pull docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
+    docker pull docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
+    docker pull docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
+    docker pull docker.pkg.github.com/plasmnetwork/zkrollups/postgres:latest
+    docker pull matterlabs/dev-ticker:latest
 
-echo "Start Integration Test..."
-docker-compose -f docker-compose.test.yml up -d substrate operator prover postgres ticker
-docker-compose -f docker-compose.test.yml up test
+    echo "Start Integration Test..."
+    docker-compose -f docker-compose.test.yml up -d substrate operator prover postgres ticker
+    docker-compose -f docker-compose.test.yml up test
+else
+    echo "Start Integration Test..."
+    docker-compose build
+    docker-compose up -d substrate operator prover postgres ticker
+    docker-compose up test
+fi
