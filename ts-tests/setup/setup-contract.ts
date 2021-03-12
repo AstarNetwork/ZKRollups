fdfaaa03eff128ea5c99a1c04ba4e54af58eab4f
NoCtrlZ <phantomofrotten@gmail.com>
2021-03-12 09:47:51 +0900

    migrate setup scripts

--- ts-tests/setup/setup-contract.ts
+++ ts-tests/setup/setup-contract.ts
@@ -4,8 +4,8 @@ import ZkSync from '../build/ZkSync.json'
 import Verifier from '../build/Verifier.json'
 import Governance from '../build/Governance.json'
 import DeployFactory from '../build/DeployFactory.json'
-import { operatorAddress, genesisRoot } from './util'
-import Utilities, { createAndFinalizeBlock, customRequest, describeWithFrontier } from "./util";
+import { operatorAddress, genesisRoot } from '../tests/util'
+import Utilities, { createAndFinalizeBlock, customRequest, describeWithFrontier } from "../tests/util";
 
 const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
 const GENESIS_ACCOUNT_PRIVATE_KEY = "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
