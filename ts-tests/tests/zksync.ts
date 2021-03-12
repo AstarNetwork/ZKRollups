02a544d8b1171f2299d26c0c8df289121aa2aa12
NoCtrlZ <phantomofrotten@gmail.com>
2021-03-12 15:20:06 +0900

    change test reference

--- ts-tests/tests/zksync.ts
+++
@@ -1,3 +0,0 @@
-export * from './zksync/sdk/zksync.js/src'
-export { sleep, ACCOUNT, PRIVATE_KEY } from './zksync/sdk/zksync.js/src/utils'
-export { Provider } from './zksync/sdk/zksync.js/src/provider'
