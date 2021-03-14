9f241b14e6b87c3785d775d191acff6d493ea61b
NoCtrlZ <phantomofrotten@gmail.com>
2021-03-14 10:12:27 +0900

    change operator and prover reference

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,12 +10,12 @@ if [ $1 = "actions" ]; then
     docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/substrate
 
     echo "Prepare Operator Container Image..."
-    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/server/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
+    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f ts-tests/tests/zksync/docker/server/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
     docker push docker.pkg.github.com/plasmnetwork/zkrollups/operator:latest
     docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/operator
 
     echo "Prepare Prover Contaienr Image..."
-    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
+    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f ts-tests/tests/zksync/docker/prover/Dockerfile zksync -t docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
     docker push docker.pkg.github.com/plasmnetwork/zkrollups/prover:latest
     docker rmi docker.pkg.github.com/plasmnetwork/zkrollups/prover
 
@@ -26,6 +26,6 @@ if [ $1 = "actions" ]; then
     docker push docker.pkg.github.com/plasmnetwork/zkrollups/ts-tests:latest
 else
     echo "Build Server And Prover..."
-    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/server/Dockerfile zksync -t zksync/server
-    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f zksync/docker/prover/Dockerfile zksync -t zksync/prover
+    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f ts-tests/tests/zksync/docker/server/Dockerfile zksync -t zksync/server
+    DOCKER_BUILDKIT=1 docker build --ssh default=$HOME/.ssh/id_rsa -f ts-tests/tests/zksync/docker/prover/Dockerfile zksync -t zksync/prover
 fi
